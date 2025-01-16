ec32facfb5de439729c01e361e0b7f9f8efa760f
Priya Pandey <129404384+priyapandey5@users.noreply.github.com>
2025-01-16 19:25:31 +0530

    Update RegisterPage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -39,7 +39,7 @@ const RegisterPage = () => {
         register_form.append(key, formData[key])
       }
 
-      const response = await fetch("https://mernback-7uzl.onrender.com/auth/register", {
+      const response = await fetch("https://homerent-oo0v.onrender.com/auth/register", {
         method: "POST",
         body: register_form
       })
